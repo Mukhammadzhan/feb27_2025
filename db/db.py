fbd6bc7b7a24df81033f587c82bd5c7082655ecf
Mukhammadzhan Abduvakhidov <abduvahidov@772mail.ru>
2025-02-27 00:07:32 +0500

    second commit

---
+++ db/db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+conn = sqlite3.connect('users.db')
+
+
+cursor = conn.cursor()
+
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name TEXT NOT NULL,
+               age INTEGER NOT NULL
+               )
+
+""")
+
+conn.commit()
+conn.close()
